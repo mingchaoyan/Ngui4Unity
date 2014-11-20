a97fb64c0aa7bcd8008a2d822df5fd6447b18e13
mingchaoyan <mingchaoyan@gmail.com>
2014-11-20 22:11:22 +0800

    toggle

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class VolumeManager : MonoBehaviour {
+	public UIToggle soundToggle;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,9 @@ public class VolumeManager : MonoBehaviour {
         slider = GetComponent<UISlider>();
         //Set the Slider's value to last saved volume
         slider.value = NGUITools.soundVolume;
+		//If volume is at 0, uncheck the Sound Checkbox
+		if(NGUITools.soundVolume == 0) 
+			soundToggle.value = false;
     }
 
     public void OnVolumeChange ()
@@ -28,5 +32,15 @@ public class VolumeManager : MonoBehaviour {
         NGUITools.soundVolume = UISlider.current.value;
         //Change the Game AudioListener's volume
         AudioListener.volume = UISlider.current.value;
+    }
+    public void OnSoundToggle()
+    {
+        float newVolume = 0;
+        //If sound toggled ON, set new volume to slider value
+        if(UIToggle.current.value)
+            newVolume = slider.value;
+        //Apply newVolume to volumes
+        AudioListener.volume = newVolume;
+        NGUITools.soundVolume = newVolume;
     }
 }
