4806e2a8c691b8bd4fad5b64df0f637070803cd8
mingchaoyan <mingchaoyan@gmail.com>
2014-11-21 20:51:39 +0800

    popup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,13 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    public enum Difficulties
+    {
+        Normal,
+        Hard
+    }
+    public static Difficulties Difficulty =  
+        Difficulties.Normal;
 
 	// Use this for initialization
 	void Start () {
@@ -17,4 +24,14 @@ public class GameManager : MonoBehaviour {
         //Exit Now
         Application.Quit();
     }
+
+    public void OnDifficultyChange()
+    {
+        //If Difficulty changes to Normal, set Difficulties.Normal
+        if(UIPopupList.current.value == "Normal")
+            Difficulty = Difficulties.Normal;
+        //Otherwise, set it to Hard
+        else Difficulty = Difficulties.Hard;
+    }
+
 }
